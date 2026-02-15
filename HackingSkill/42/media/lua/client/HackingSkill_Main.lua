27883c6d9c80587e9694816c32ac76da894bea3e
Jusblazm <ryanlarkin0@gmail.com>
2026-02-15 14:52:48 -0800

    v2.5.4 - updated legacy version warning

--- HackingSkill/42/media/lua/client/HackingSkill_Main.lua
+++ HackingSkill/42/media/lua/client/HackingSkill_Main.lua
@@ -1,6 +1,6 @@
 -- HackingSkill_Main
 local function updateWarning(playerIndex, playerObj)
-    print("[EZPZBanking] Warning: This version of Hacking Skill is no longer supported. Please update to the newest version to get new features and continued support.")
+    print("[HackingSkill] Warning: This version of Hacking Skill is no longer supported. Please update to the newest version to get new features and continued support.")
 end
 
 Events.OnCreatePlayer.Add(updateWarning)
