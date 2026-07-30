60924d46ec91295b1f896cca0751a1ea2e46ff3b
Jusblazm <ryanlarkin0@gmail.com>
2026-07-29 17:07:18 -0700

    v2.7 - legacy build 42.19 release, removed other 42.xx builds

--- HackingSkill/42.15/media/lua/client/HackingSkill_ClientHandler.lua
+++
@@ -1,9 +0,0 @@
-Events.OnServerCommand.Add(function(module, command, args)
-    if module == "HackingSkill" and command == "BypassResult" then
-        if args.success then
-            getPlayer():Say(getText("IGUI_HackingSkill_PlayerText_DoorUnlockSuccess"))
-        else
-            getPlayer():Say(getText("IGUI_HackingSkill_PlayerText_DoorUnlockFailed"))
-        end
-    end
-end)
--- HackingSkill/42/media/lua/client/HackingSkill_Main.lua
+++
@@ -1,6 +0,0 @@
--- HackingSkill_Main
-local function updateWarning(playerIndex, playerObj)
-    print("[HackingSkill] Warning: This version of Hacking Skill is no longer supported. Please update to the newest version to get new features and continued support.")
-end
-
-Events.OnCreatePlayer.Add(updateWarning)
